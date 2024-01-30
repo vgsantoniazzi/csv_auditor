9a8144d3422f27a56aa12e4a50b0ec197e0c8b8e
Victor Antoniazzi <vgsantoniazzi@gmail.com>
2024-01-30 11:09:00 -0300

    Add proper e-mail validation

---
+++ lib/csv_auditor/validator/email.rb
@@ -0,0 +1,16 @@
+module CsvAuditor
+  class Validator
+    class Email
+      def self.validate(row:, fields:, name:, processed_rows:, options: {})
+        raise ArgumentError, "only allowed one field validation for format" if fields.length > 1
+
+        email = row[fields.first]
+        email = email&.strip if options["strip"]
+
+        return if email =~ URI::MailTo::EMAIL_REGEXP
+
+        row[name] = "#{fields.first}: #{email} does not match regex #{URI::MailTo::EMAIL_REGEXP}"
+      end
+    end
+  end
+end
