49c56635d3e6aaa998195035563d81414aea9c74
Victor Antoniazzi <vgsantoniazzi@gmail.com>
2024-02-14 09:47:49 -0300

    Allow more than one file per configuration

--- lib/csv_auditor/configuration.rb
+++ lib/csv_auditor/configuration.rb
@@ -15,20 +15,17 @@ module CsvAuditor
   end
 
   class Configuration
-    attr_accessor :config_file_path, :output, :validations, :validators
+    attr_accessor :config_file_path, :output, :executions, :validators
 
-    EXPECTED_COLUMNS = %w[name validation columns].freeze
+    EXPECTED_EXECUTION_COLUMNS = %w[file output validations].freeze
+    EXPECTED_VALIDATION_COLUMNS = %w[name validation columns].freeze
 
     def config_file_path
       @config_file_path ||= ".csv_auditor.yml"
     end
 
-    def output
-      @output ||= "audited.csv"
-    end
-
-    def validations
-      @validations ||= load_validations!
+    def executions
+      @executions ||= load_executions!
     end
 
     def validators
@@ -37,23 +34,32 @@ module CsvAuditor
 
     private
 
-    def load_validations!
-      validations = YAML.load_file(config_file_path)["validations"]
-      validations.each do |validation|
-        EXPECTED_COLUMNS.each do |column|
-          raise "Configuration invalid! Missing #{column}" unless validation[column]
+    def load_executions!
+      executions = YAML.load_file(config_file_path)
+
+      executions.each do |execution|
+        EXPECTED_EXECUTION_COLUMNS.each do |column|
+          raise "Configuration invalid! Missing #{column}" unless execution[column]
+
+          execution["validations"].each do |validation|
+            EXPECTED_VALIDATION_COLUMNS.each do |column|
+              raise "Configuration invalid! Missing #{column}" unless validation[column]
+            end
+          end
         end
       end
 
-      validations
+      executions
     end
 
     def load_validators!
       validators = CsvAuditor::Validator.new
-      validations.each do |validation|
-        loadable_module = validation["validation"]
+      executions.each do |execution|
+        execution["validations"].each do |validation|
+          loadable_module = validation["validation"]
 
-        validators.add(loadable_module) unless validators.validators[loadable_module]
+          validators.add(loadable_module) unless validators.validators[loadable_module]
+        end
       end
 
       validators
